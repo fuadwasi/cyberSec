e104055c3e1c4a87e956916336da434a24be95e4
fuadwasi <fuad15-9400@diu.edu.bd>
2019-10-21 12:04:47 +0600

    CyberSec APP

---
+++ app/src/main/java/com/example/cybersec/Common_Activity.java
@@ -0,0 +1,14 @@
+package com.example.cybersec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Common_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common);
+    }
+}
